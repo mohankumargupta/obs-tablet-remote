a1cbf262e9886a2e3d98c9b0fac8c378a23928b3
mohankumargupta <mohangupta@live.com>
2018-04-18 18:32:52 +1000

    simplify home screen

--- src/components/views/home.vue
+++ src/components/views/home.vue
@@ -4,43 +4,6 @@
 			<div class="home-connect">
 				<connect @update:query="val => this.urlArgs = val" />
 			</div>
-			<div class="home-info">
-				<h1>OBS Tablet Remote</h1>
-
-				<p>Control <a href="https://obsproject.com/" target="_blank">OBS Studio</a> over the network, on any device with a web browser</p>
-
-				<div class="alert warning" v-if="showHTTPSWarning">
-					<h2>Warning: HTTPS Detected</h2>
-
-					<p>It appears like you have loaded this page over a secure connection. While security is cool, obs websocket plugins don't support it and browsers will block the connection. <a :href="httpURL">Click here to attempt to load http version of this page</a></p>
-				</div>
-
-				<p>To use OBS Tablet Remote make sure you have installed the <a href="https://obsproject.com/forum/resources/websocket-plugin.466/" target="_blank">Websocket Plugin</a> for OBS Studio</p>
-
-				<h2>Automatic Login</h2>
-
-				<p>To automatically connect, bookmark this URL with connection info in URL after #!auto (default values can be skipped)</p>
-
-				<label>
-					Current settings:
-					<input
-						type="text"
-						readonly
-						:value="exampleURL"
-						@click="selectValue"
-					>
-				</label>
-
-				<p>To include password add &password={password}. Note that all fields can't include &amp; or =.</p>
-
-				<h2>Layout Editor</h2>
-
-				<p>Make the remote just the way you like it! Now you can add, remove and resize panels all you want!</p>
-
-				<h2>Bugs / Feature requests?</h2>
-
-				<a href="https://github.com/t2t2/obs-tablet-remote/issues" target="_blank">Github Issues</a>
-			</div>
 		</div>
 	</div>
 </template>
